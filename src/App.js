3ad9c2af13d05ad606fd7ae9ca50494565966842
Javiuty <javiiescuadra@gmail.com>
2021-08-26 00:30:28 +0200

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { About } from "./components/About";
 import { AddTask } from "./components/AddTask";
+import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Tasks } from "./components/Tasks";
 
@@ -21,6 +24,14 @@ function App() {
   // Fetch Tasks
   const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json();
+
+    return data
+  }
+
+  // Fetch Tasks
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
     const data = await res.json();
 
     return data
@@ -53,17 +64,40 @@ function App() {
   }
 
   // Toggle Reminder
-  const toggleReminder = id => {
-    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  const toggleReminder = async id => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json();
+
+
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task))
   }
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
 
-    </div>
+        <Route path="/" exact render={(props) => (
+          <>
+            {showAddTask && <AddTask onAdd={addTask} />}
+            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
+          </>
+        )} />
+        <Route path="/about" component={About} />
+        <Footer />
+      </div>
+    </Router>
+
   );
 }
 
